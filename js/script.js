66c38dd35ca7e124ae302f4c1e71825f3922e19c
LEEYJ <youngji1223@naver.com>
2022-05-10 21:30:46 +0900

    2022-05-10

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
  
-    $(".slider").slick({
+    $(".slider").slick({ 
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: true,
+       
+    })
+
+        $(".btn-box >.center").click(function(){
+    
+            let num = $(this).index()
+                
+           $('.slider').slick('slickGoTo' , num );
+          })
 
 
- })
+        $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            
+            $(".center").text(nextSlide+1 + " / 8")
+        });
+
+
+        $(".btn-box >.prev").click(function(){
+           $(".slider").slick("slickPrev")
+        })
+
+        $(".btn-box >.next").click(function(){
+           $(".slider").slick("slickNext")
+            
+          });
 
  
 
-//  끝
+//// 끝
 })
